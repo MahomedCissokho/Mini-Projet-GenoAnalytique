f7a0d0cd5bc82c77029a9f8132b66ce72cad29c9
fatima6299 <123902203+fatima6299@users.noreply.github.com>
2024-04-16 12:06:48 +0200

    Update Surface.java

--- src/geoanalytique/model/Surface.java
+++ src/geoanalytique/model/Surface.java
@@ -5,9 +5,9 @@ import java.util.List;
  * Représente une surface dans un plan cartésien.
  * Une surface est définie par une liste de points qui la délimitent.
  * Cette classe est abstraite et sert de classe de base pour d'autres surfaces spécifiques.
- * @author Abdallah Victor Assolofo Aicha
- * @author Nana Cathy Aidara
- * @author Mohamed Cissoko
+ * @author Abdallah Solofo Victor Aicha
+ * @author Nana Katy Aidara
+ * @author Mohamed Cissokho
  * @author Fatimata Tidiane Dia
  * @author Bassirou Kane
  */
