2a8182c0499982c7f5360c9d35c24e7157b4d26b
fatima6299 <123902203+fatima6299@users.noreply.github.com>
2024-04-16 12:13:22 +0200

    Update GeoObject.java

--- src/geoanalytique/model/GeoObject.java
+++ src/geoanalytique/model/GeoObject.java
@@ -3,10 +3,10 @@ package geoanalytique.model;
  * Représente un objet géométrique dans un plan cartésien.
  * Cette classe est abstraite et sert de classe de base pour d'autres objets géométriques spécifiques.
  * @author Abdallah Solofo Victor Aicha
- * @author Nana Cathy Aidara
- * @author Mohamed Cissoko
+ * @author Nana Katy Aidara
+ * @author Mohamed Cissokho
  * @author Fatimata Tidiane Dia
- * @author Bassirou Kane
+ * @author Bassirou Kane
  */
 
 public abstract class GeoObject {
