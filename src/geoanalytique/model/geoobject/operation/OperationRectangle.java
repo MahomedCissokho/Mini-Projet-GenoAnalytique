f354b2e6a74144e2fc2b9722b967d4f1f8fba18b
nakaty <nanakatyaidara@esp.sn>
2024-04-16 22:36:30 +0000

    monderniercommit

--- src/geoanalytique/model/geoobject/operation/OperationRectangle.java
+++ src/geoanalytique/model/geoobject/operation/OperationRectangle.java
@@ -1,4 +1,17 @@
 package geoanalytique.model.geoobject.operation;
+import geoanalytique.model.Rectangle;
+import geoanalytique.util.Operation;
+/**
+ * La classe Rectangle contient les methodes ...
+ * @author Abdallah Solofo Victor AICHA,Nana Katy AIDARA, Mahomed CISSOKHO, 
+ * Fatima Tidiane DIA, Bassirou KANE
+ */
 
-public class OperationRectangle {
+public class OperationRectangle implements Operation {
+    Rectangle rectangle;
+
+/**
+ * @param 
+ */
+    
 }
