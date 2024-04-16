dbec7aeb256aad888318309d231dd90b669e8886
nakaty <nanakatyaidara@esp.sn>
2024-04-16 22:44:08 +0000

    monderniercommit

--- src/geoanalytique/model/geoobject/operation/OperationCercle.java
+++ src/geoanalytique/model/geoobject/operation/OperationCercle.java
@@ -1,4 +1,101 @@
 package geoanalytique.model.geoobject.operation;
 
-public class OperationCercle {
+import geoanalytique.model.Cercle;
+import geoanalytique.model.Droite;
+import geoanalytique.util.Operation;
+
+/**
+ * La classe OperationCercle contient les méthodes spécifiques aux opérations sur les cercles.
+ * Elle implémente l'interface Operation pour définir les opérations disponibles sur un cercle.
+ * 
+ * @author Abdallah Solofo Victor AICHA, Nana Katy AIDARA, Mahomed CISSOKHO,
+ *         Fatima Tidiane DIA, Bassirou KANE
+ */
+public class OperationCercle implements Operation {
+    private Cercle cercle;
+
+    /**
+     * Constructeur de la classe OperationCercle.
+     * Il initialise l'objet OperationCercle avec le cercle donné en paramètre.
+     * 
+     * @param cercle Le cercle sur lequel les opérations seront effectuées.
+     */
+    public OperationCercle(Cercle cercle) {
+        this.cercle = cercle;
+    }
+
+    /**
+     * Méthode pour calculer le périmètre du cercle.
+     * 
+     * @return Le périmètre du cercle.
+     */
+    public double calculerPerimetre() {
+        return 2 * Math.PI * cercle.getRayon();
+    }
+
+    /**
+     * Méthode pour calculer la surface du cercle.
+     * 
+     * @return La surface du cercle.
+     */
+    public double calculerSurface() {
+        return Math.PI * cercle.getRayon() * cercle.getRayon();
+    }
+ /**
+     * Cette methode renvoie le nom de l'operation 
+     * @return "OperationCercle"
+     */
+    
+     public String getTitle() {
+        return "OperationCercle";
+    }
+
+    /**
+     * Cette methode renvoie le nombre d'attributs de la classe
+     * @return 1
+     */
+    public int getArite() {
+        return 1;
+    }
+
+    /**
+     * Cette methode permet de changer l'attribut Cercle.
+     * @param num de type object
+     * @param o de type object
+     */
+    public void setArgument(int num, Object o) {
+        if (num == 1 && o instanceof Cercle) {
+            this.cercle = (Cercle) o;
+        }
+        
+    }
+
+    /**
+     * Cette methode permet de retourner le nom de l'attribut
+     * @return "le nom de l'attribut"
+     */
+    public String getDescriptionArgument(int num) {
+        if (num == 1)
+            return "cercle";
+
+        else return "";
+
+    }
+    /**
+     * Cette methode permet de retourner la classe de l'attribut
+     * @param num de type int
+     * @return "La classe de l'attribut"
+     */
+
+    public class<?> getClassArgument(int num) {
+        if (num == 1)
+            return Cercle.Class;
+    }
+
+    /**
+     * Cette methode permet de...
+     */
+    public Object calculer(){
+
+    }
 }
--- src/geoanalytique/model/geoobject/operation/OperationRectangle.java
+++ src/geoanalytique/model/geoobject/operation/OperationRectangle.java
@@ -2,16 +2,97 @@ package geoanalytique.model.geoobject.operation;
 import geoanalytique.model.Rectangle;
 import geoanalytique.util.Operation;
 /**
- * La classe Rectangle contient les methodes ...
+ * La classe OperationRectangle contient les méthodes spécifiques aux opérations sur les rectangles.
+ * 
  * @author Abdallah Solofo Victor AICHA,Nana Katy AIDARA, Mahomed CISSOKHO, 
  * Fatima Tidiane DIA, Bassirou KANE
  */
 
 public class OperationRectangle implements Operation {
     Rectangle rectangle;
+    /**
+     * Constructeur de la classe OperationRectangle.
+     * Il initialise l'objet OperationRectangle avec le rectangle donné en paramètre.
+     * 
+     * @param rectangle Le rectangle sur lequel les opérations seront effectuées.
+     */
+    public OperationRectangle(Rectangle rectangle) {
+        this.rectangle = rectangle;
+    }
+     /**
+     * Méthode pour calculer l'aire du rectangle.
+     * 
+     * @return L'aire du rectangle.
+     */
+    public double calculerAire() {
+        return rectangle.getLongueur() * rectangle.getLargeur();
+    }
 
-/**
- * @param 
- */
+    /**
+     * Méthode pour calculer le périmètre du rectangle.
+     * 
+     * @return Le périmètre du rectangle.
+     */
+    public double calculerPerimetre() {
+        return 2 * (rectangle.getLongueur() + rectangle.getLargeur());
+    }
+
+     /**
+     * Cette methode renvoie le nom de l'operation 
+     * @return "OperationRectangle"
+     */
+    
+     public String getTitle() {
+        return "OperationRectangle";
+    }
+
+    /**
+     * Cette methode renvoie le nombre d'attributs de la classe
+     * @return 1
+     */
+    public int getArite() {
+        return 1;
+    }
+
+    /**
+     * Cette methode permet de changer l'attribut Rectangle.
+     * @param num de type object
+     * @param o de type object
+     */
+    public void setArgument(int num, Object o) {
+        if (num == 1 && o instanceof Rectangle) {
+            this.rectangle = (Rectangle) o;
+        }
+        
+    }
+
+    /**
+     * Cette methode permet de retourner le nom de l'attribut
+     * @return "le nom de l'attribut"
+     */
+    public String getDescriptionArgument(int num) {
+        if (num == 1)
+            return "rectangle";
+
+        else return "";
+
+    }
+    /**
+     * Cette methode permet de retourner la classe de l'attribut
+     * @param num de type int
+     * @return "La classe de l'attribut"
+     */
+
+    public class<?> getClassArgument(int num) {
+        if (num == 1)
+            return Rectangle.Class;
+    }
+
+    /**
+     * Cette methode permet de...
+     */
+    public Object calculer(){
+
+    }
     
 }
