842f68ebd825f41d3d4123c93eb23e8d94423b72
fatima6299 <123902203+fatima6299@users.noreply.github.com>
2024-04-16 12:02:48 +0200

    Update Polygone.java

--- src/geoanalytique/model/Polygone.java
+++ src/geoanalytique/model/Polygone.java
@@ -4,7 +4,7 @@ import java.util.List;
  * Représente un polygone dans un plan cartésien.
  * Un polygone est défini par un nombre de côtés et une liste de sommets.
  * Cette classe est abstraite et sert de classe de base pour d'autres polygones spécifiques.
- * @author Abdallah Victor Assolofo Aicha
+ * @author Abdallah Solofo Victor Aicha
  * @author Nana Cathy Aidara
  * @author Mohamed Cissoko
  * @author Fatimata Tidiane Dia
